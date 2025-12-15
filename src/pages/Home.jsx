90f5dc67c331bddff0d3d1b1f42b538900890540
Mahmoud-Abdelnasser20 <mxtreamer60@gmail.com>
2025-12-15 19:17:34 +0200

    fixed images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Star from "../components/Star";
+import avatar from "/src/assets/avataaars.svg";
 
 export default function Home() {
     return (
         <div className="min-vh-100 bg-color-tertiary d-flex justify-content-center align-items-center flex-column">
             <div className="image d-flex justify-content-center align-items-center">
-                <img className="home-image w-75" src="src/assets/avataaars.svg" alt="" />
+                <img className="home-image w-75" src={avatar} alt="" />
             </div>
             <h2 className="text-white my-3 fw-bold">START FRAMEWORK</h2>
             <Star />
